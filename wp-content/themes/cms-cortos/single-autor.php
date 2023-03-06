c6350af6eedf16150c3784ffd674d529cbf1e0c0
NahuelUTNPigui <nahuel.n.piguillem@gmail.com>
2023-03-05 23:50:46 -0600

    try 1

--- wp-content/themes/cms-cortos/single-autor.php
+++ wp-content/themes/cms-cortos/single-autor.php
@@ -4,9 +4,11 @@
 <head>
   <!-- Import default project head content -->
   <?php wp_head(); ?>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </head>
 
-<body>
+<body style="background-color: black;color: #FFF;">
 
   <!-- Import header -->
   <?php get_header(); ?>
@@ -15,6 +17,18 @@
   <!-- Page content -->
   <main>
     <h1>Single Autor</h1>
+    <div class="container">
+      <div class="row">
+        <div class="col">1</div>
+        <div class="col">2</div>
+      </div>
+      <div class="row">
+        <div class="col">1</div>
+        <div class="col">2</div>
+        <div class="col">3</div>
+      </div>
+    </div>
+
   </main>
   <!-- End page content -->
 
